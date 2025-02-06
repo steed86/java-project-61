95b331199fde342e291937298007bdeaad6157bb
Michael Tikhonov <tixonovms@yandex.ru>
2025-02-06 19:49:01 +0300

    modified greeting in Cli class and Engine

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,7 +6,7 @@ public class Cli {
     public static String askNameAndGreet() {
         System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("May i have your name? ");
+        System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         return userName;
